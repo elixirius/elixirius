cbc7262dbeba35e3c1505ca3f2ea7d82cab98984
Volodya Sveredyuk <sveredyuk@gmail.com>
2020-11-13 14:02:33 +0200

    Github actions setup

--- config/test.exs
+++ config/test.exs
@@ -15,6 +15,12 @@ config :elixirius, Elixirius.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+if System.get_env("GITHUB_ACTIONS") do
+ config :elixirius, Elixirius.Repo,
+   username: "postgres",
+   password: "postgres"
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :elixirius, ElixiriusWeb.Endpoint,
